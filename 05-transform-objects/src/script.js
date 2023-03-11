b821a564d68fba1eb4fef10c42aaf3cbcd24210a
JenLim <hj.jen.lim@gmail.com>
2023-03-12 01:51:15 +0900

    Scene graph

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -9,28 +9,38 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const group = new THREE.Group();
+group.scale.y = 0.5;
+group.rotation.y = 0.5;
+group.position.y = 2;
+scene.add(group);
 
-// Position (x,y,z)
-mesh.position.set(0.6, -0.7, 1);
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "yellow" })
+);
+group.add(cube1);
 
-// Scale
-mesh.scale.set(2, 0.5, 0.5);
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "red" })
+);
+cube2.position.x = -2;
+group.add(cube2);
 
-// Rotation
-// mesh.rotation.reorder("YXZ");
-mesh.rotation.x = Math.PI * 0.25;
-mesh.rotation.y = Math.PI * 0.25;
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "skyblue" })
+);
+cube3.position.x = 2;
+group.add(cube3);
 
 // Axes Helper
 const axesHelper = new THREE.AxesHelper(2);
 scene.add(axesHelper);
 
 // distance from the center to the object
-console.log(mesh.position.length());
+// console.log(mesh.position.length());
 
 /*
 // reduce the vector until it becomes 1.
@@ -56,9 +66,9 @@ camera.position.z = 3;
 scene.add(camera);
 
 // distance from the camera to the object
-console.log(mesh.position.distanceTo(camera.position));
+// console.log(mesh.position.distanceTo(camera.position));
 
-camera.lookAt(mesh.position);
+// camera.lookAt(mesh.position);
 
 /**
  * Renderer
