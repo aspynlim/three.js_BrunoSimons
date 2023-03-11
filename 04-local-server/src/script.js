1bb71dc23d1d8220aa75c9a9ff2cdb6800e0b99e
JenLim <hj.jen.lim@gmail.com>
2023-03-11 22:38:12 +0900

    completed Ch 1-4

---
+++ 04-local-server/src/script.js
@@ -0,0 +1,28 @@
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+
+/* Mesh: Geometry(shape) + Material(how it looks) */
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "yellow" });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+/* Sizes */
+const sizes = {
+  width: 600,
+  height: 400,
+};
+
+/* Camera (field of view(fov), ratio) */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+/* Renderer */
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
