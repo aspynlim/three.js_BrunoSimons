54c24501199ba2c78bd27acf9192ccb5661ff1b6
JenLim <hj.jen.lim@gmail.com>
2023-03-12 19:59:36 +0900

    Handle fullscreen (including Safari)

--- 08-fullscreen-and-resizing/src/script.js
+++ 08-fullscreen-and-resizing/src/script.js
@@ -40,6 +40,25 @@ window.addEventListener("resize", () => {
   renderer.setPixelRatio(Math.min(window.setPixelRatio, 2));
 });
 
+window.addEventListener("dblclick", () => {
+  const fullscreenElement =
+    document.fullscreenElement || document.webkitFullscreenElement;
+
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen();
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen();
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+});
+
 /**
  * Camera
  */
