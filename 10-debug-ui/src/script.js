2a33e80fc6ebf33d7d4eed995fefa733c3385c7d
JenLim <hj.jen.lim@gmail.com>
2023-03-13 21:50:32 +0900

    Debug exercises

--- 10-debug-ui/src/script.js
+++ 10-debug-ui/src/script.js
@@ -5,6 +5,21 @@ import * as dat from "lil-gui";
 console.log(dat);
 
 /**
+ * Debug
+ */
+const gui = new dat.GUI();
+
+const parameters = {
+  color: 0xff0000,
+  spin: () => {
+    gsap.to(mesh.rotation, {
+      duration: 1,
+      y: mesh.rotation.y + 10,
+    });
+  },
+};
+
+/**
  * Base
  */
 // Canvas
@@ -17,9 +32,16 @@ const scene = new THREE.Scene();
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const material = new THREE.MeshBasicMaterial({ color: parameters.color });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+
+// Debug
+gui.add(mesh.position, "y").min(-3).max(3).step(0.01).name("elevation");
+gui.add(mesh, "visible");
+gui.add(material, "wireframe");
+gui.addColor(material, "color");
+gui.add(parameters, "spin");
 
 /**
  * Sizes
