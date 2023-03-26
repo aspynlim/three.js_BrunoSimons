e934d654204fef8d4a3da4c5565b6780dcce7efb
JenLim <hj.jen.lim@gmail.com>
2023-03-26 12:50:57 +0900

    Center the text

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -16,6 +16,10 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
+// Axes helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
+
 /**
  * Textures
  */
@@ -37,6 +41,14 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
     bevelOffset: 0,
     bevelSegments: 4,
   });
+  // textGeometry.computeBoundingBox();
+  // textGeometry.translate(
+  //   -(textGeometry.boundingBox.max.x - 0.02) * 0.5,
+  //   -(textGeometry.boundingBox.max.y - 0.02) * 0.5,
+  //   -(textGeometry.boundingBox.max.z - 0.02) * 0.5
+  // );
+  textGeometry.center();
+
   const textMaterial = new THREE.MeshBasicMaterial({ wireframe: true });
   const text = new THREE.Mesh(textGeometry, textMaterial);
   scene.add(text);
