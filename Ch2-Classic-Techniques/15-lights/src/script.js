3716c84665635a7201d42da4166d251fecc6c0b9
deepsales <hj.jen.lim@gmail.com>
2023-04-29 22:32:10 +0900

    Performance

--- Ch2-Classic-Techniques/15-lights/src/script.js
+++ Ch2-Classic-Techniques/15-lights/src/script.js
@@ -17,28 +17,34 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
+// Minimal Cost - AmbientLight
 // color, intensity
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
 scene.add(ambientLight)
 
 // gui.add(ambientLight, 'intensity').min(0).max(1).step(0.01)
 
+// Moderate Cost - DirectionalLight
 const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.5)
 directionalLight.position.set(1, 0.25, 0)
 scene.add(directionalLight)
 
+// Minimal Cost - HemisphereLight
 const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3)
 scene.add(hemisphereLight)
 
+// Moderate Cost - PointLight
 const pointLight = new THREE.PointLight(0xff9000, 0.5, 10, 2)
 pointLight.position.set(1, -0.5, 1)
 scene.add(pointLight)
 
+// High Cost - RectAreaLight
 const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 1)
 rectAreaLight.position.set(-1.5, 0, 1.5)
 rectAreaLight.lookAt(new THREE.Vector3())
 scene.add(rectAreaLight)
 
+// High Cost - SpotLight
 const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 10, Math.PI * 0.1, 0.25, 1)
 spotLight.position.set(0,2,3)
 scene.add(spotLight)
